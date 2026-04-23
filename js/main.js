0590d327e078735e776038cebcd00fcf955d5b39
shadespider <jariase80987654321@gmail.com>
2026-04-22 23:09:26 -0500

    sección de filtros agregada

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 function mostrarInicio() {
-    document.getElementById("contenido").innerHTML = "<h2>Bienvenido</h2><p>Explora nuestros autos</p>";
+    document.getElementById("contenido").innerHTML = "<h2>Bienvenido</h2>";
 }
 
 function mostrarCatalogo() {
-    document.getElementById("contenido").innerHTML = "<h2>Catálogo</h2><p>Aquí irán los autos</p>";
+    document.getElementById("contenido").innerHTML = "<h2>Catálogo</h2>";
 }
 
 function mostrarContacto() {
-    document.getElementById("contenido").innerHTML = "<h2>Contacto</h2><p>Escríbenos</p>";
+    document.getElementById("contenido").innerHTML = "<h2>Contacto</h2>";
+}
+
+function applyFilters() {
+    alert("Filtros aplicados (lógica vendrá después)");
+}
+
+function reset() {
+    document.getElementById("brandFilter").value = "";
+    document.getElementById("typeFilter").value = "";
 }
